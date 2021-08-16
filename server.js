41eefbbda3f6be2dfdb97e0fb197f18cfded6744
Kevin98 <kevinryner@yahoo.co.uk>
2021-08-16 21:17:11 +0100

    added post functionality, posts save to json file ad an array

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 var uniqid = require('uniqid');
 const path = require('path');
-const db = require("./db/db.json")
+const db = require("./db/db.json");
+const fs = require("fs");
+const { json } = require("express");
 
 const PORT = 3001;
 const app = express();
+app.use(express.json());
 
 app.use(express.static('public'));
 
@@ -17,9 +20,47 @@ app.get(`/api/notes`, (req,res) =>
   res.json(db)
 );
 
-// app.post(`api/notes`, (req,res) =>
+app.post(`/api/notes`, (req,res) => {
+  console.log(`${req.method} request received`)
 
-// );
+   // Destructuring assignment for the items in req.body
+   const { title, text } = req.body;
+
+   // If all the required properties are present
+   if (title && text) {
+     // Variable for the object we will save
+     const newNote = {
+       title,
+       text,
+       note_id: uniqid(),
+     };
+ 
+     // Convert the data to a string so we can save it
+     var notesArray = db;
+     notesArray.push(newNote);
+     var notesArrayString = JSON.stringify(notesArray);
+     
+
+     fs.writeFile(`./db/db.json`, notesArrayString, `utf8`, (err) =>
+       err
+         ? console.error(err)
+         : console.log(
+             `Note ${newNote.title} has been written to JSON file`
+           )
+     );
+ 
+     const response = {
+       status: 'success',
+       body: newNote,
+     };
+ 
+     console.log(response);
+     res.json(response);
+   } else {
+     res.json('Error in posting note');
+   }
+
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
